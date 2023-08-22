6f1c04c4db7efdd4a366544142bb07b00e0514f8
Nick Eisenberg <nickeisenberg@gmail.com>
2023-08-21 17:33:27 -0700

    qu 6

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ own machine, %100 locally. The generation of a database will give the user the
 opportunity to become familiar with
 
 * Python and SQL integration,
-* SQLAlchemy,
 * creation of a SQL server, ie mysql, postgresql, sqlite, etc.,
-* connection the SQL to SQLAlchemy through engines,
+* SQLAlchemy,
+* connecting the SQL server to SQLAlchemy through engines,
 * SQL syntax.
 
-Often the case, online tutorial-sites have a built-in SQL interpreter and so 
-only the last bullet point above is addressed. Below are the listed
-subdirectories that correspond to a database generator.
+Often, online tutorial-sites have a built-in SQL interpreter as well
+as built in databases and so only the last bullet point above is 
+addressed. Below are the listed subdirectories that correspond to a 
+database generator.
 
 ## Current database generators
 1. parents_and_children
--- parents_and_children/queries/qu6.sql
+++ parents_and_children/queries/qu6.sql
@@ -1,3 +1,41 @@
 -- Suppose that all of the childen who have jobs are earning %30 of what the
 -- average of their parents are earning. List the names of the top three children
 -- according to their salaries.
+
+with 
+    par1 as (
+        select 
+            child_id, 
+            parent1_id, 
+            salary as p1sal
+        from children as c
+        left join employment as e
+        on c.parent1_id = e.parent_id
+        where is_employed = 1
+    ),
+    par2 as (
+        select 
+            child_id, 
+            parent2_id, 
+            salary as p2sal
+        from children as c
+        left join employment as e
+        on c.parent2_id = e.parent_id
+        where is_employed = 1
+    ),
+    child_salary_rank as (
+        select 
+            par1.child_id, 
+            .3 * (p1sal + p2sal) / 2 as salary,
+            dense_rank() over ( order by .3 * (p1sal + p2sal) / 2 desc) as ranking
+        from par1
+        left join par2
+        on par1.child_id = par2.child_id 
+    )
+select first_name, last_name, cr.salary
+from children as c
+left join child_salary_rank as cr
+on c.child_id = cr.child_id
+where cr.ranking <= 3
+;
+
